17e7b5e4cd29dbee3b43fd69d7cef0903e162b46
pyw0k <adrianpe92@gmail.com>
2019-12-08 21:09:26 -0800

    Decided to move elements from app.py to newly created core.py. core.py now deals with initializing, fetching, and exception raising.

---
+++ core.py
@@ -0,0 +1,49 @@
+import requests
+
+class CoinMarketCap:
+	BASE_URL = 'https://pro-api.coinmarketcap.com'
+
+	def __init__(self, api_key, version='v1', fetch_timeout=None):
+		self.api_key = api_key
+		self.version = version
+		self.fetch_timeout = fetch_timeout
+
+		self.url = '{}/{}'.format(self.BASE_URL, version)
+		self.session = self._init_session()
+
+	def _init_session(self):
+		session - requests.Session()
+		session.headers.update({
+			'Accepts' : 'application/json',
+			'X-CMC_PRO_API_KEY': self.api_key,
+			})
+
+		return session
+
+	def fetch(self, path, params=None):
+		response = self.session.get(
+			'{}{}'.format(self.url, path),
+			params=params,
+			timeout=self.fetch_timeout)
+
+		return self.parse_response(response)
+
+	def parse_response(response):
+		response_dict = response.json()
+		if response.status_code == 200:
+			return response_dict['data']
+		else:
+			error_message = response_dict['status']['error_message']
+
+			if response.status_code == 400:
+				raise BadRequestException(error_message)
+			elif response.status_code == 401:
+				raise UnauthorizedException(error_message)
+			elif response.status_code == 402:
+				raise PaymentRequiredException(error_message)
+			elif response.status_code == 403:
+				raise ForbiddenException(error_message)
+			elif response.status_code == 429:
+				raise TooManyRequestsException(error_message)
+			else:
+				raise InternalServerErrorException(error_message)
