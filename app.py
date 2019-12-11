ef8242371f5b3f210c1bbd5518f087df7e8ab942
pyw0k <adrianpe92@gmail.com>
2019-12-11 13:46:33 -0800

    Learning how to properly work with API

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ currencies = ['USD', 'ALL', 'DZD', 'ARS', 'AMD', 'AUD', 'AZN', 'BHD', 'BDT',
 'ZAR', 'KRW', 'SSP', 'VES', 'LKR', 'SEK', 'CHF', 'THB', 'TTD', 'TND', 'TRY',
 'UGX', 'UAH', 'AED', 'UYU', 'UZS', 'VND']
 
-def convert_fiat(crypto, fiat):
+def convert_fiat(top, fiat):
 	if fiat:
 		if fiat in currencies:
-			url = f"https://api.coinmarketcap.com/v1/ticker/?convert={fiat}&limit={crypto}"
+			url = f"https://api.coinmarketcap.com/v1/ticker/?convert={fiat}&limit={top}"
 		else:
 			print("Error: Not a valid currency pair")
 			sys.exit(1)
 	else:
-		url = f"https://api.coinmarketcap.com/v1/ticker/?limit={crypto}"
+		url = f"https://api.coinmarketcap.com/v1/ticker/?limit={top}"
 
 	date = datetime.now().time()
 	print(f"Fetched data from coinmarketcap.com at {date}")
