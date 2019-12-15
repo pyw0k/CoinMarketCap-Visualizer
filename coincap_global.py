a8cdb088768009e13f1f33c59831a30a6890e979
pyw0k <adrianpe92@gmail.com>
2019-12-14 16:47:59 -0800

    Reconiled changes

--- coincap_global.py
+++ coincap_global.py
@@ -10,12 +10,10 @@ active_currencies = results['data']['active_cryptocurrencies']
 active_markets = results['data']['active_markets']
 bitcoin_percentage = results['data']['bitcoin_percentage_of_market_cap']
 last_updated = results['data']['last_updated']
-global_cap = results['data']['quotes']['total_market_cap']
 global_volume = results['data']['quotes']['USD']['total_volume_24h']
 
 print()
 print('There are currently ' + str(active_currencies) + ' active currencies and ' + str(active_markets) + '.')
-print('The global cap of all cryptos is ' + str(global_cap) + ' and the 24h global volume is ' + str(global_volume) + '.')
-print('Bitcoin\'s total percentage of the global cap is ' + bitcoin_percentage + '.')
+print('Bitcoin\'s total percentage of the global cap is ' + str(bitcoin_percentage) + '.')
 print()
 print('This information was last updated on ' + str(last_updated) + '.')
